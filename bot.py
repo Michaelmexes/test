b143b11dab864661b3fa899d74d68cbfcb10fe6a
Michaelmexes <90118122+Michaelmexes@users.noreply.github.com>
2026-06-09 11:33:28 +0630

    bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,20 @@
 import os
-import google.generativeai as genai
+from google import genai
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 GEMINI_KEY = os.environ.get("GEMINI_KEY")
 
-genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+client = genai.Client(api_key=GEMINI_KEY)
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_msg = update.message.text
     try:
-        response = model.generate_content(user_msg)
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=user_msg
+        )
         await update.message.reply_text(response.text)
     except Exception as e:
         await update.message.reply_text("မေးခွန်းထပ်ပို့ပါ၊ ပြဿနာတစ်ခုဖြစ်သွားသည်။")
